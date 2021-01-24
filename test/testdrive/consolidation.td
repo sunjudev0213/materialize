sunjudev0213/materialize

commit 6dc1689b6778c8774dbd263b2f50c1b40b49272b

Refactor sinks to make them "dumber"; enable upsert sinks (#5028)

src/dataflow/src/render/mod.rs
@@ -98,20 +98,22 @@
 //! stream. This reduces the amount of recomputation that must be performed
 //! if/when the errors are retracted.
 
-use std::any::Any;
 use std::collections::{HashMap, HashSet};
 use std::iter;
 use std::rc::Rc;
 use std::rc::Weak;
+use std::{any::Any, cell::RefCell};
 
-use differential_dataflow::hashable::Hashable;
 use differential_dataflow::lattice::Lattice;
 use differential_dataflow::operators::arrange::arrangement::{Arrange, ArrangeByKey};
 use differential_dataflow::operators::arrange::upsert::arrange_from_upsert;
-use differential_dataflow::operators::consolidate::Consolidate;
+
+use differential_dataflow::hashable::Hashable;
 use differential_dataflow::{AsCollection, Collection};
 use futures::executor::block_on;
 
+use interchange::envelopes::{combine_at_timestamp, dbz_format, upsert_format};
+use repr::adt::decimal::Significand;
 use timely::dataflow::operators::to_stream::ToStream;
 use timely::dataflow::operators::unordered_input::UnorderedInput;
 use timely::dataflow::operators::Map;
@@ -119,6 +121,7 @@ use timely::dataflow::scopes::Child;
 use timely::dataflow::Scope;
 
 use timely::communication::Allocate;
+use timely::dataflow::operators::exchange::Exchange;
 use timely::worker::Worker as TimelyWorker;
 
 use dataflow_types::*;
@@ -128,7 +131,7 @@ use mz_avro::Schema;
 use ore::cast::CastFrom;
 use ore::collections::CollectionExt as _;
 use ore::iter::IteratorExt;
-use repr::{Datum, RelationType, Row, RowArena, Timestamp};
+use repr::{Datum, RelationType, Row, RowArena, RowPacker, Timestamp};
 
 use crate::decode::{decode_avro_values, decode_values};
 use crate::operator::{CollectionExt, StreamExt};
@@ -227,7 +230,7 @@ pub fn build_dataflow<A: Allocate>(
 
             // Export declared sinks.
             for (sink_id, sink) in &dataflow.sink_exports {
-                let imports = dataflow.get_imports(&sink.from.0);
+                let imports = dataflow.get_imports(&sink.from);
                 context.export_sink(render_state, imports, *sink_id, sink);
             }
         });
@@ -328,7 +331,7 @@ where
                     caching_tx,
                 };
 
-                let capability = if let Envelope::Upsert(key_encoding) = envelope {
+                let capability = if let SourceEnvelope::Upsert(key_encoding) = envelope {
                     match connector {
                         ExternalSourceConnector::Kafka(_) => {
                             let (source, capability) = source::create_source::<_, KafkaSourceInfo, _>(
@@ -456,8 +459,8 @@ where
                     };
 
                     let mut collection = match envelope {
-                        Envelope::None | Envelope::CdcV2 => stream.as_collection(),
-                        Envelope::Debezium(_) =>
+                        SourceEnvelope::None | SourceEnvelope::CdcV2 => stream.as_collection(),
+                        SourceEnvelope::Debezium(_) =>
                         // TODO(btv) -- this should just be a RelationExpr::Explode (name TBD)
                         {
                             stream.as_collection().explode({
@@ -469,7 +472,7 @@ where
                                 }
                             })
                         }
-                        Envelope::Upsert(_) => unreachable!(),
+                        SourceEnvelope::Upsert(_) => unreachable!(),
                     };
 
                     // Implement source filtering and projection.
@@ -689,37 +692,134 @@ where
         }
         let (collection, _err_collection) = self
             .collection(&RelationExpr::global_get(
-                sink.from.0,
-                sink.from.1.typ().clone(),
+                sink.from,
+                sink.from_desc.typ().clone(),
             ))
             .expect("Sink source collection not loaded");
+
+        // Some connectors support keys - extract them.
+        let key_indices = sink
+            .connector
+            .get_key_indices()
+            .map(|key_indices| key_indices.to_vec());
+        let keyed = collection.map(move |row| {
+            let key = key_indices.as_ref().map(|key_indices| {
+                // TODO[perf] (btv) - is there a way to avoid unpacking and repacking every row and cloning the datums?
+                // Does it matter?
+                let datums = row.unpack();
+                Row::pack(key_indices.iter().map(|&idx| datums[idx].clone()))
+            });
+            (key, row)
+        });
+
+        // Each partition needs to be handled by its own worker, so that we can write messages in order.
+        // For now, we only support single-partition sinks.
+        let keyed = keyed
+            .inner
+            .exchange(move |_| sink_id.hashed())
+            .as_collection();
+
+        // Apply the envelope.
+        // * "Debezium" consolidates the stream, sorts it by time, and produces DiffPairs from it.
+        //   It then renders those as Avro.
+        // * Upsert" does the same, except at the last step, it renders the diff pair in upsert format.
+        //   (As part of doing so, it asserts that there are not multiple conflicting values at the same timestamp)
+        // * "Tail" writes some metadata.
+        let collection = match sink.envelope {
+            SinkEnvelope::Debezium => {
+                let combined = combine_at_timestamp(keyed.arrange_by_key().stream);
+                // This has to be an `Rc<RefCell<...>>` because the inner closure (passed to `Iterator::map`) references it, and it might outlive the outer closure.
+                let rp = Rc::new(RefCell::new(RowPacker::new()));
+                let collection = combined.flat_map(move |(mut k, v)| {
+                    let max_idx = v.len() - 1;
+                    let rp = rp.clone();
+                    v.into_iter().enumerate().map(move |(idx, dp)| {
+                        let k = if idx == max_idx { k.take() } else { k.clone() };
+                        (k, Some(dbz_format(&mut *rp.borrow_mut(), dp)))
+                    })
+                });
+                collection
+            }
+            SinkEnvelope::Upsert => {
+                let combined = combine_at_timestamp(keyed.arrange_by_key().stream);
+
+                let collection = combined.map(|(k, v)| {
+                    let v = upsert_format(v);
+                    (k, v)
+                });
+                collection
+            }
+            SinkEnvelope::Tail { emit_progress } => keyed
+                .inner
+                .map({
+                    let mut rp = RowPacker::new();
+                    move |((k, v), time, diff)| {
+                        rp.push(Datum::Decimal(Significand::new(i128::from(time))));
+                        if emit_progress {
+                            rp.push(Datum::False);
+                        }
+                        rp.push(Datum::Int64(i64::cast_from(diff)));
+                        rp.extend_by_row(&v);
+                        let v = rp.finish_and_reuse();
+                        ((k, Some(v)), time, 1)
+                    }
+                })
+                .as_collection(),
+        };
+
+        // Some sinks require that the timestamp be appended to the end of the value.
+        let append_timestamp = match &sink.connector {
+            SinkConnector::Kafka(c) => c.consistency.is_some(),
+            SinkConnector::Tail(_) => false,
+            SinkConnector::AvroOcf(_) => false,
+        };
+        let collection = if append_timestamp {
+            collection
+                .inner
+                .map(|((k, v), t, diff)| {
+                    let v = v.map(|v| {
+                        let mut rp = RowPacker::new();
+                        rp.extend_by_row(&v);
+                        let t = t.to_string();
+                        rp.push_list_with(|rp| {
+                            rp.push(Datum::String(&t));
+                        });
+                        rp.finish()
+                    });
+                    ((k, v), t, diff)
+                })
+                .as_collection()
+        } else {
+            collection
+        };
 
         // TODO(benesch): errors should stream out through the sink,
         // if we figure out a protocol for that.
 
-        // TODO(frank): consolidation is only required for a collection,
-        // not for arrangements. We can perform a more complicated match
-        // here to determine which case we are in to avoid this call.
-        let collection = collection.consolidate();
-
         match sink.connector.clone() {
             SinkConnector::Kafka(c) => {
-                let token = sink::kafka(&collection.inner, sink_id, c, sink.from.1.clone());
+                let token = sink::kafka(
+                    collection,
+                    sink_id,
+                    c,
+                    sink.key_desc.clone(),
+                    sink.value_desc.clone(),
+                );
                 needed_sink_tokens.push(token);
             }
             SinkConnector::Tail(c) => {
-                // Map by sink_id is not needed for correctness, but will spread the work
-                // around when there are multiple TAILs running, and additionally will move a
-                // single TAIL's work to a single worker.
-                // Arranging will cause updates to be presented in time order.
-                let stream = collection
-                    .map(move |row| (sink_id, row))
+                let batches = collection
+                    .map(move |(k, v)| {
+                        assert!(k.is_none(), "tail does not support keys");
+                        let v = v.expect("tail must have values");
+                        (sink_id, v)
+                    })
                     .arrange_by_key()
                     .stream;
-                sink::tail(stream, sink_id, c);
+                sink::tail(batches, sink_id, c);
             }
             SinkConnector::AvroOcf(c) => {
-                sink::avro_ocf(&collection.inner, sink_id, c, sink.from.1.clone());
+                sink::avro_ocf(collection, sink_id, c, sink.value_desc.clone());
             }
         };
 

src/dataflow/src/decode/mod.rs
@@ -27,7 +27,7 @@ use timely::{
 
 use ::mz_avro::{types::Value, Schema};
 use dataflow_types::LinearOperator;
-use dataflow_types::{DataEncoding, Envelope, RegexEncoding};
+use dataflow_types::{DataEncoding, RegexEncoding, SourceEnvelope};
 use interchange::avro::{extract_row, ConfluentAvroResolver, DebeziumDecodeState, DiffPair};
 use log::error;
 use repr::Datum;
@@ -45,7 +45,7 @@ mod regex;
 
 pub fn decode_avro_values<G>(
     stream: &Stream<G, SourceOutput<Vec<u8>, Value>>,
-    envelope: &Envelope,
+    envelope: &SourceEnvelope,
     schema: Schema,
     debug_name: &str,
 ) -> Stream<G, (Row, Timestamp, Diff)>
@@ -57,7 +57,7 @@ where
     // so that we can spread the decoding among all the workers.
     // See #2133
     let envelope = envelope.clone();
-    let mut dbz_state = if let Envelope::Debezium(dedup_strat) = envelope {
+    let mut dbz_state = if let SourceEnvelope::Debezium(dedup_strat) = envelope {
         DebeziumDecodeState::new(
             &schema,
             debug_name.to_string(),
@@ -80,13 +80,13 @@ where
         )| {
             let top_node = schema.top_node();
             let diffs = match envelope {
-                Envelope::None => {
+                SourceEnvelope::None => {
                     extract_row(value, index.map(Datum::from), top_node).map(|r| DiffPair {
                         before: None,
                         after: r,
                     })
                 }
-                Envelope::Debezium(_) => {
+                SourceEnvelope::Debezium(_) => {
                     if let Some(dbz_state) = dbz_state.as_mut() {
                         dbz_state.extract(value, top_node, index, upstream_time_millis)
                     } else {
@@ -95,8 +95,8 @@ where
                         ))
                     }
                 }
-                Envelope::Upsert(_) => unreachable!("Upsert is not supported for AvroOCF"),
-                Envelope::CdcV2 => unreachable!("CDC envelope is not supported for AvroOCF"),
+                SourceEnvelope::Upsert(_) => unreachable!("Upsert is not supported for AvroOCF"),
+                SourceEnvelope::CdcV2 => unreachable!("CDC envelope is not supported for AvroOCF"),
             }
             .unwrap_or_else(|e| {
                 // TODO(#489): Handle this in a better way,
@@ -485,7 +485,7 @@ pub fn decode_values<G>(
     stream: &Stream<G, SourceOutput<Vec<u8>, Vec<u8>>>,
     encoding: DataEncoding,
     debug_name: &str,
-    envelope: &Envelope,
+    envelope: &SourceEnvelope,
     // Information about optional transformations that can be eagerly done.
     // If the decoding elects to perform them, it should replace this with
     // `None`.
@@ -502,24 +502,24 @@ where
         .ok()
         .and_then(|desc| desc.typ().keys.get(0).cloned());
     match (encoding, envelope) {
-        (_, Envelope::Upsert(_)) => {
+        (_, SourceEnvelope::Upsert(_)) => {
             unreachable!("Internal error: Upsert is not supported yet on non-Kafka sources.")
         }
-        (DataEncoding::Csv(enc), Envelope::None) => (
+        (DataEncoding::Csv(enc), SourceEnvelope::None) => (
             csv(stream, enc.header_row, enc.n_cols, enc.delimiter, operators),
             None,
         ),
-        (DataEncoding::Avro(enc), Envelope::CdcV2) => {
+        (DataEncoding::Avro(enc), SourceEnvelope::CdcV2) => {
             decode_cdcv2(stream, &enc.value_schema, enc.schema_registry_config)
         }
-        (_, Envelope::CdcV2) => {
+        (_, SourceEnvelope::CdcV2) => {
             unreachable!("Internal error: CDCv2 is not supported yet on non-Avro sources.")
         }
-        (DataEncoding::Avro(enc), Envelope::Debezium(_)) => {
+        (DataEncoding::Avro(enc), SourceEnvelope::Debezium(_)) => {
             // can't get this from the above match arm because:
             // `error[E0658]: binding by-move and by-ref in the same pattern is unstable`
             let dedup_strat = match envelope {
-                Envelope::Debezium(ds) => *ds,
+                SourceEnvelope::Debezium(ds) => *ds,
                 _ => unreachable!(),
             };
 
@@ -565,13 +565,13 @@ where
         (DataEncoding::AvroOcf { .. }, _) => {
             unreachable!("Internal error: Cannot decode Avro OCF separately from reading")
         }
-        (_, Envelope::Debezium(_)) => unreachable!(
+        (_, SourceEnvelope::Debezium(_)) => unreachable!(
             "Internal error: A non-Avro Debezium-envelope source should not have been created."
         ),
-        (DataEncoding::Regex(RegexEncoding { regex }), Envelope::None) => {
+        (DataEncoding::Regex(RegexEncoding { regex }), SourceEnvelope::None) => {
             (regex_fn(stream, regex, debug_name), None)
         }
-        (DataEncoding::Protobuf(enc), Envelope::None) => (
+        (DataEncoding::Protobuf(enc), SourceEnvelope::None) => (
             decode_values_inner(
                 stream,
                 protobuf::ProtobufDecoderState::new(&enc.descriptors, &enc.message_name),
@@ -580,7 +580,7 @@ where
             ),
             None,
         ),
-        (DataEncoding::Bytes, Envelope::None) => (
+        (DataEncoding::Bytes, SourceEnvelope::None) => (
             decode_values_inner(
                 stream,
                 OffsetDecoderState::from(bytes_to_datum),
@@ -589,7 +589,7 @@ where
             ),
             None,
         ),
-        (DataEncoding::Text, Envelope::None) => (
+        (DataEncoding::Text, SourceEnvelope::None) => (
             decode_values_inner(
                 stream,
                 OffsetDecoderState::from(text_to_datum),

src/coord/src/catalog.rs
@@ -14,6 +14,7 @@ use std::time::SystemTime;
 
 use anyhow::bail;
 use chrono::{DateTime, TimeZone, Utc};
+use dataflow_types::SinkEnvelope;
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use log::{info, trace};
@@ -163,6 +164,7 @@ pub struct Sink {
     pub plan_cx: PlanContext,
     pub from: GlobalId,
     pub connector: SinkConnectorState,
+    pub envelope: SinkEnvelope,
     pub with_snapshot: bool,
     pub as_of: Option<u64>,
 }
@@ -1528,6 +1530,7 @@ impl Catalog {
                 plan_cx: pcx,
                 from: sink.from,
                 connector: SinkConnectorState::Pending(sink.connector_builder),
+                envelope: sink.envelope,
                 with_snapshot,
                 as_of,
             }),
@@ -1810,7 +1813,7 @@ impl ExprHumanizer for ConnCatalog<'_> {
                 self.humanize_scalar_type(&ScalarType::String),
                 self.humanize_scalar_type(value_type)
             ),
-            Record { fields } => format!(
+            Record { fields, .. } => format!(
                 "record({})",
                 fields
                     .iter()

src/coord/src/coord.rs
@@ -27,6 +27,7 @@ use std::thread::{self, JoinHandle};
 use std::time::{Duration, SystemTime};
 
 use anyhow::{anyhow, bail, Context};
+use dataflow_types::SinkEnvelope;
 use differential_dataflow::lattice::Lattice;
 use futures::future::{self, TryFutureExt};
 use futures::sink::SinkExt;
@@ -1037,6 +1038,7 @@ where
             id,
             sink.from,
             connector,
+            sink.envelope,
         ))
         .await
     }
@@ -1542,6 +1544,7 @@ where
                 copy_to,
                 emit_progress,
                 object_columns,
+                desc,
             } => tx.send(
                 self.sequence_tail(
                     &session,
@@ -1551,6 +1554,7 @@ where
                     copy_to,
                     emit_progress,
                     object_columns,
+                    desc,
                 )
                 .await,
                 session,
@@ -1852,6 +1856,7 @@ where
                 plan_cx: pcx,
                 from: sink.from,
                 connector: catalog::SinkConnectorState::Pending(sink.connector_builder.clone()),
+                envelope: sink.envelope,
                 with_snapshot,
                 as_of,
             }),
@@ -2263,6 +2268,7 @@ where
         copy_to: Option<CopyFormat>,
         emit_progress: bool,
         object_columns: usize,
+        desc: RelationDesc,
     ) -> Result<ExecuteResponse, anyhow::Error> {
         // Determine the frontier of updates to tail *from*.
         // Updates greater or equal to this frontier will be produced.
@@ -2288,7 +2294,9 @@ where
                 strict: !with_snapshot,
                 emit_progress,
                 object_columns,
+                value_desc: desc,
             }),
+            SinkEnvelope::Tail { emit_progress },
         ))
         .await?;
 

src/coord/src/coord/dataflow_builder.rs
@@ -169,12 +169,13 @@ impl<'a> DataflowBuilder<'a> {
         id: GlobalId,
         from: GlobalId,
         connector: SinkConnector,
+        envelope: SinkEnvelope,
     ) -> DataflowDesc {
         let mut dataflow = DataflowDesc::new(name);
         dataflow.set_as_of(connector.get_frontier());
         self.import_into_dataflow(&from, &mut dataflow);
         let from_type = self.catalog.get_by_id(&from).desc().unwrap().clone();
-        dataflow.add_sink_export(id, from, from_type, connector);
+        dataflow.add_sink_export(id, from, from_type, connector, envelope);
         dataflow
     }
 }

src/coord/src/sink_connector.rs
@@ -144,12 +144,13 @@ async fn build_kafka(
         value_schema_id,
         topic,
         addrs: builder.broker_addrs,
+        key_desc_and_indices: builder.key_desc_and_indices,
+        value_desc: builder.value_desc,
         consistency,
         fuel: builder.fuel,
         frontier,
         strict: !with_snapshot,
         config_options: builder.config_options,
-        key_indices: builder.key_indices,
     }))
 }
 
@@ -193,5 +194,6 @@ fn build_avro_ocf(
         path,
         frontier,
         strict: !with_snapshot,
+        value_desc: builder.value_desc,
     }))
 }

src/coord/src/timestamp.rs
@@ -38,9 +38,9 @@ use aws_util::kinesis;
 use dataflow::source::read_file_task;
 use dataflow::source::FileReadStyle;
 use dataflow_types::{
-    AvroOcfEncoding, Consistency, DataEncoding, Envelope, ExternalSourceConnector,
-    FileSourceConnector, KafkaSourceConnector, KinesisSourceConnector, MzOffset, S3SourceConnector,
-    SourceConnector, TimestampSourceUpdate,
+    AvroOcfEncoding, Consistency, DataEncoding, ExternalSourceConnector, FileSourceConnector,
+    KafkaSourceConnector, KinesisSourceConnector, MzOffset, S3SourceConnector, SourceConnector,
+    SourceEnvelope, TimestampSourceUpdate,
 };
 use expr::{PartitionId, SourceInstanceId};
 use ore::collections::CollectionExt;
@@ -712,8 +712,8 @@ fn is_ts_valid(
 /// 5) any source that uses the Avro format currently expects a consistency source that is formatted
 /// using the BYO_CONSISTENCY_SCHEMA Avro spec outlined above.
 ///
-fn identify_consistency_format(enc: DataEncoding, env: Envelope) -> ConsistencyFormatting {
-    if let Envelope::Debezium(_) = env {
+fn identify_consistency_format(enc: DataEncoding, env: SourceEnvelope) -> ConsistencyFormatting {
+    if let SourceEnvelope::Debezium(_) = env {
         if let DataEncoding::AvroOcf(AvroOcfEncoding { reader_schema: _ }) = enc {
             ConsistencyFormatting::DebeziumOcf
         } else {
@@ -1238,7 +1238,7 @@ impl Timestamper {
         id: SourceInstanceId,
         sc: ExternalSourceConnector,
         enc: DataEncoding,
-        env: Envelope,
+        env: SourceEnvelope,
         timestamp_topic: String,
     ) -> Option<ByoTimestampConsumer> {
         match sc {

src/dataflow-types/src/types.rs
@@ -188,12 +188,19 @@ impl DataflowDesc {
         from_id: GlobalId,
         from_desc: RelationDesc,
         connector: SinkConnector,
+        envelope: SinkEnvelope,
     ) {
+        let key_desc = connector.get_key_desc().cloned();
+        let value_desc = connector.get_value_desc().clone();
         self.sink_exports.push((
             id,
             SinkDesc {
-                from: (from_id, from_desc),
+                from: from_id,
+                from_desc,
+                key_desc,
+                value_desc,
                 connector,
+                envelope,
             },
         ));
     }
@@ -238,7 +245,7 @@ impl DataflowDesc {
             result.extend(self.get_imports(&desc.on_id))
         }
         for (_, sink) in &self.sink_exports {
-            result.extend(self.get_imports(&sink.from.0))
+            result.extend(self.get_imports(&sink.from))
         }
         result
     }
@@ -298,11 +305,11 @@ pub enum DataEncoding {
 impl DataEncoding {
     /// Computes the [`RelationDesc`] for the relation specified by the this
     /// data encoding and envelope.s
-    pub fn desc(&self, envelope: &Envelope) -> Result<RelationDesc, anyhow::Error> {
+    pub fn desc(&self, envelope: &SourceEnvelope) -> Result<RelationDesc, anyhow::Error> {
         // Add columns for the key, if using the upsert envelope.
         let key_desc = match envelope {
-            Envelope::Upsert(key_encoding) => {
-                let key_desc = key_encoding.desc(&Envelope::None)?;
+            SourceEnvelope::Upsert(key_encoding) => {
+                let key_desc = key_encoding.desc(&SourceEnvelope::None)?;
 
                 // It doesn't make sense for the key to have keys.
                 assert!(key_desc.typ().keys.is_empty());
@@ -457,25 +464,36 @@ impl SourceDesc {
 /// A sink for updates to a relational collection.
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct SinkDesc {
-    pub from: (GlobalId, RelationDesc),
+    pub from: GlobalId,
+    pub from_desc: RelationDesc,
+    pub value_desc: RelationDesc,
+    pub key_desc: Option<RelationDesc>,
     pub connector: SinkConnector,
+    pub envelope: SinkEnvelope,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum SinkEnvelope {
+    Debezium,
+    Upsert,
+    Tail { emit_progress: bool },
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum Envelope {
+pub enum SourceEnvelope {
     None,
     Debezium(DebeziumDeduplicationStrategy),
     Upsert(DataEncoding),
     CdcV2,
 }
 
-impl Envelope {
+impl SourceEnvelope {
     pub fn get_avro_envelope_type(&self) -> avro::EnvelopeType {
         match self {
-            Envelope::None => avro::EnvelopeType::None,
-            Envelope::Debezium { .. } => avro::EnvelopeType::Debezium,
-            Envelope::Upsert(_) => avro::EnvelopeType::Upsert,
-            Envelope::CdcV2 => avro::EnvelopeType::CdcV2,
+            SourceEnvelope::None => avro::EnvelopeType::None,
+            SourceEnvelope::Debezium { .. } => avro::EnvelopeType::Debezium,
+            SourceEnvelope::Upsert(_) => avro::EnvelopeType::Upsert,
+            SourceEnvelope::CdcV2 => avro::EnvelopeType::CdcV2,
         }
     }
 }
@@ -485,7 +503,7 @@ pub enum SourceConnector {
     External {
         connector: ExternalSourceConnector,
         encoding: DataEncoding,
-        envelope: Envelope,
+        envelope: SourceEnvelope,
         consistency: Consistency,
         ts_frequency: Duration,
     },
@@ -663,6 +681,8 @@ pub struct KafkaSinkConsistencyConnector {
 pub struct KafkaSinkConnector {
     pub addrs: KafkaAddrs,
     pub topic: String,
+    pub key_desc_and_indices: Option<(RelationDesc, Vec<usize>)>,
+    pub value_desc: RelationDesc,
     pub key_schema_id: Option<i32>,
     pub value_schema_id: i32,
     pub consistency: Option<KafkaSinkConsistencyConnector>,
@@ -672,11 +692,11 @@ pub struct KafkaSinkConnector {
     pub frontier: Antichain<Timestamp>,
     pub strict: bool,
     pub config_options: BTreeMap<String, String>,
-    pub key_indices: Option<Vec<usize>>,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct AvroOcfSinkConnector {
+    pub value_desc: RelationDesc,
     pub path: PathBuf,
     pub frontier: Antichain<Timestamp>,
     pub strict: bool,
@@ -690,6 +710,33 @@ impl SinkConnector {
             SinkConnector::Tail(tail) => tail.frontier.clone(),
         }
     }
+
+    pub fn get_key_desc(&self) -> Option<&RelationDesc> {
+        match self {
+            SinkConnector::Kafka(k) => k.key_desc_and_indices.as_ref().map(|(desc, _indices)| desc),
+            SinkConnector::Tail(_) => None,
+            SinkConnector::AvroOcf(_) => None,
+        }
+    }
+
+    pub fn get_key_indices(&self) -> Option<&[usize]> {
+        match self {
+            SinkConnector::Kafka(k) => k
+                .key_desc_and_indices
+                .as_ref()
+                .map(|(_desc, indices)| indices.as_slice()),
+            SinkConnector::Tail(_) => None,
+            SinkConnector::AvroOcf(_) => None,
+        }
+    }
+
+    pub fn get_value_desc(&self) -> &RelationDesc {
+        match self {
+            SinkConnector::Kafka(k) => &k.value_desc,
+            SinkConnector::Tail(t) => &t.value_desc,
+            SinkConnector::AvroOcf(a) => &a.value_desc,
+        }
+    }
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
@@ -699,6 +746,7 @@ pub struct TailSinkConnector {
     pub strict: bool,
     pub emit_progress: bool,
     pub object_columns: usize,
+    pub value_desc: RelationDesc,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
@@ -711,13 +759,17 @@ pub enum SinkConnectorBuilder {
 pub struct AvroOcfSinkConnectorBuilder {
     pub path: PathBuf,
     pub file_name_suffix: String,
+    pub value_desc: RelationDesc,
 }
 
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct KafkaSinkConnectorBuilder {
     pub broker_addrs: KafkaAddrs,
     pub schema_registry_url: Url,
+    pub key_schema: Option<String>,
     pub value_schema: String,
+    pub key_desc_and_indices: Option<(RelationDesc, Vec<usize>)>,
+    pub value_desc: RelationDesc,
     pub topic_prefix: String,
     pub topic_suffix: String,
     pub replication_factor: u32,
@@ -725,8 +777,6 @@ pub struct KafkaSinkConnectorBuilder {
     pub consistency_value_schema: Option<String>,
     pub config_options: BTreeMap<String, String>,
     pub ccsr_config: ccsr::ClientConfig,
-    pub key_indices: Option<Vec<usize>>,
-    pub key_schema: Option<String>,
 }
 
 /// An index storing processed updates so they can be queried

commit 362d0d22e675a1f505a4170a90092afb56950b3e

dataflow: consolidate data flowing into TAIL sinks
Consolidation ensures that TAIL's initial snapshot does not contain
records that cancel. This regressed in #5028 because we didn't have test
coverage.

Fix #5421.

src/dataflow/src/render/mod.rs
@@ -749,6 +749,7 @@ where
                 collection
             }
             SinkEnvelope::Tail { emit_progress } => keyed
+                .consolidate()
                 .inner
                 .map({
                     let mut rp = RowPacker::new();

test/testdrive/consolidation.td
@@ -113,6 +113,17 @@ $ kafka-ingest format=avro topic=tx schema=${tx-schema}
 $ kafka-verify format=avro sink=materialize.public.nums_sink
 {"before": null, "after": {"row": {"num": 5}}}
 
+# Validate that `TAIL` is similarly consolidated.
+# This protects against regression of #5421.
+
+> BEGIN
+> DECLARE cur CURSOR FOR TAIL nums AS OF 3
+> FETCH ALL cur
+timestamp  diff  num
+--------------------
+3          1     5
+> COMMIT
+
 # ==> Test compaction.
 
 # Each transaction that has been updated so far should be separately visible