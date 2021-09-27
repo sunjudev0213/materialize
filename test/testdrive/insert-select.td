sunjudev0213/materialize

commit b99aec29714d761b5223c1ec4ee77c2cab563c61

sql: support subqueries in UPDATE/DELETE WHERE clause
Because UPDATE/DELETE block any writes between their peek time
and their commit time, allowing subqueries in their WHERE clauses
causes no serializability violations.

test/testdrive/update.td
@@ -54,6 +54,15 @@ invalid input syntax for type double precision
 > SELECT * FROM t ORDER BY i
 4 6 be
 
+> CREATE TABLE o (i int);
+> INSERT INTO o VALUES (1), (2), (3), (4);
+
+# Support subqueries
+> UPDATE t SET t = 'xy' WHERE i IN (SELECT i FROM o)
+
+> SELECT * FROM t
+4 6 xy
+
 > CREATE VIEW v AS SELECT 1 a;
 
 ! UPDATE v SET a = 1
@@ -106,7 +115,7 @@ cannot be run inside a transaction block
 > COMMIT
 
 > SELECT * FROM t ORDER BY i
-4 6 be
+4 6 xy
 <null> <null> <null>
 <null> <null> <null>
 

src/sql/src/plan/query.rs
@@ -720,7 +720,7 @@ pub fn plan_mutation_query(
                 ),
                 relation_type: &RelationType::new(desc.iter_types().cloned().collect()),
                 allow_aggregates: false,
-                allow_subqueries: false,
+                allow_subqueries: true,
             };
             let expr = plan_expr(&ecx, &expr)?.type_as(&ecx, &ScalarType::Bool)?;
             HirRelationExpr::Filter {

commit 72b0d741a0511351c9e732b402d7d6d094c5d592

coord: restrict table mutation selections to other user tables
To ensure serializability, we need to ensure ReadThenWritePlans
never refer to objects that can receive writes between the plan's
read and its write. The only objects for which we can make this
guarantee are user-defined tables, which we lock for the length
of the operation.

Note that this patch also fixes a bug introduced in b99aec2,
which let UPDATE/DELETE use subqueries, which introduced
potential serializability violations.

src/coord/src/coord.rs
@@ -3361,17 +3361,13 @@ impl Coordinator {
     ) -> Result<ExecuteResponse, CoordError> {
         let catalog = self.catalog.for_session(session);
         let values = sql::plan::plan_copy_from(&session.pcx(), &catalog, id, columns, rows)?;
-        let plan = InsertPlan {
-            id,
-            values: values.lower(),
-        };
 
         let constants = self
-            .prep_relation_expr(plan.values, ExprPrepStyle::Write)?
+            .prep_relation_expr(values.lower(), ExprPrepStyle::Write)?
             .into_inner();
 
         // Copied rows must always be constants.
-        self.sequence_insert_constant(session, plan.id, constants)
+        self.sequence_insert_constant(session, id, constants)
     }
 
     // ReadThenWrite is a plan whose writes depend on the results of a
@@ -3407,6 +3403,20 @@ impl Coordinator {
                 return;
             }
         };
+
+        // Ensure selection targets are valid, i.e. user-defined tables, or
+        // objects local to the dataflow.
+        for id in selection.global_uses() {
+            let valid = match self.catalog.try_get_by_id(id) {
+                // TODO: Widen this check when supporting temporary tables.
+                Some(entry) if id.is_user() => entry.is_table(),
+                _ => false,
+            };
+            if !valid {
+                tx.send(Err(CoordError::InvalidTableMutationSelection), session);
+                return;
+            }
+        }
 
         let ts = self.get_read_ts();
         let peek_response = match self.sequence_peek(

src/coord/src/error.rs
@@ -43,6 +43,8 @@ pub enum CoordError {
     InvalidAlterOnDisabledIndex(String),
     /// The value for the specified parameter does not have the right type.
     InvalidParameterType(&'static (dyn Var + Send + Sync)),
+    /// The selection value for a table mutation operation refers to an invalid object.
+    InvalidTableMutationSelection,
     /// Expression violated a column's constraint
     ConstraintViolation(NotNullViolation),
     /// The named operation cannot be run in a transaction.
@@ -217,6 +219,9 @@ impl fmt::Display for CoordError {
                 p.name().quoted(),
                 p.type_name().quoted()
             ),
+            CoordError::InvalidTableMutationSelection => {
+                f.write_str("invalid selection: operation may only refer to user-defined tables")
+            }
             CoordError::ConstraintViolation(not_null_violation) => {
                 write!(f, "{}", not_null_violation)
             }

src/pgwire/src/message.rs
@@ -348,6 +348,7 @@ impl ErrorResponse {
             CoordError::IdExhaustionError => SqlState::INTERNAL_ERROR,
             CoordError::IncompleteTimestamp(_) => SqlState::SQL_STATEMENT_NOT_YET_COMPLETE,
             CoordError::InvalidParameterType(_) => SqlState::INVALID_PARAMETER_VALUE,
+            CoordError::InvalidTableMutationSelection => SqlState::INVALID_TRANSACTION_STATE,
             CoordError::ConstraintViolation(NotNullViolation(_)) => SqlState::NOT_NULL_VIOLATION,
             CoordError::OperationProhibitsTransaction(_) => SqlState::ACTIVE_SQL_TRANSACTION,
             CoordError::OperationRequiresTransaction(_) => SqlState::NO_ACTIVE_SQL_TRANSACTION,

test/testdrive/insert-select.td
@@ -72,6 +72,20 @@ cannot be run inside a transaction block
 
 > COMMIT
 
+> CREATE MATERIALIZED VIEW v (a, b, c) AS SELECT 11, 12::real, 'f';
+
+! INSERT INTO t SELECT * FROM v;
+invalid selection
+
+# Table check descends into select targets
+! INSERT INTO t (i, f, t) SELECT column1, column2, column3
+    FROM ( VALUES (11, 12, 'f') )
+    LEFT JOIN (
+        SELECT a, b, c FROM v
+    ) AS y
+    ON y.a = column1
+invalid selection
+
 # Multiple connections
 
 > CREATE TABLE c (a int);

test/testdrive/update.td
@@ -63,7 +63,11 @@ invalid input syntax for type double precision
 > SELECT * FROM t
 4 6 xy
 
-> CREATE VIEW v AS SELECT 1 a;
+# Update subqueries can only reference other tables
+> CREATE MATERIALIZED VIEW v (a) AS SELECT 4;
+
+! UPDATE t SET i = i + 1 WHERE i IN (SELECT a FROM v);
+invalid selection
 
 ! UPDATE v SET a = 1
 cannot mutate view