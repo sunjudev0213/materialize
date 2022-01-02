sunjudev0213/materialize

commit 1e869d68e414e235313e53120a968be03cc6053d

mzcompose: improve error message output
Introduce a new UIError exception type that can nicely format an error
message along with a hint, and use it everywhere. This new exception
type subsumes the existing exception hierarchy in the `errors` module;
there's no point in having a complicated exception hierarchy when they
all ultimately result in the same error message being printed to the
console.

misc/python/materialize/ui.py
@@ -15,7 +15,10 @@ import os
 import shlex
 import sys
 import time
+from contextlib import contextmanager
 from typing import Any, AsyncGenerator, Callable, Generator, Iterable, Optional, Union
+
+from colored import attr, fg
 
 from materialize import docker
 
@@ -183,3 +186,44 @@ def humanize(val: Union[int, float], kind: str = "B") -> str:
         index += 1
     suffix = suffixes[index]
     return f"{val:.1f} {suffix}{kind}"
+
+
+class UIError(Exception):
+    """An error intended for display to humans.
+
+    Use this exception type when the error is something the user can be expected
+    to handle. If the error indicates a truly unexpected condition (i.e., a
+    programming error), use a different exception type that will produce a
+    backtrace instead.
+
+    Attributes:
+        hint: An optional hint to display alongside the error message.
+    """
+
+    def __init__(self, message: str, hint: Optional[str] = None):
+        super().__init__(message)
+        self.hint = hint
+
+    def set_hint(self, hint: str) -> None:
+        """Attaches a hint to the error.
+
+        This method will overwrite the existing hint, if any.
+        """
+        self.hint = hint
+
+
+@contextmanager
+def error_handler(prog: str) -> Any:
+    """Catches and pretty-prints any raised `UIError`s.
+
+    Args:
+        prog: The name of the program with which to prefix the error message.
+    """
+    try:
+        yield
+    except UIError as e:
+        print(f"{prog}: {fg('red')}error:{attr('reset')} {e}", file=sys.stderr)
+        if e.hint:
+            print(f"{attr('bold')}hint:{attr('reset')} {e.hint}")
+    except KeyboardInterrupt:
+        sys.exit(1)

misc/python/colored.pyi
@@ -1,0 +1,12 @@
+# Copyright Materialize, Inc. and contributors. All rights reserved.
+#
+# Use of this software is governed by the Business Source License
+# included in the LICENSE file at the root of this repository.
+#
+# As of the Change Date specified in that file, in accordance with
+# the Business Source License, use of this software will be governed
+# by the Apache License, Version 2.0.
+
+def fg(color: str) -> str: ...
+def bg(color: str) -> str: ...
+def attr(attr: str) -> str: ...

misc/python/materialize/cli/mkrelease.py
@@ -19,7 +19,8 @@ import click
 import requests
 from semver.version import Version
 
-from materialize import errors, git, spawn, ui
+from materialize import git, spawn, ui
+from materialize.ui import UIError
 
 BIN_CARGO_TOML = "src/materialized/Cargo.toml"
 LICENSE = "LICENSE"
@@ -85,9 +86,7 @@ def new_rc(
     new_version = None
     if level == "rc":
         if tag.prerelease is None or not tag.prerelease.startswith("rc"):
-            raise errors.MzConfigurationError(
-                "Attempted to bump an rc version without starting an RC"
-            )
+            raise UIError("Attempted to bump an rc version without starting an RC")
         next_rc = int(tag.prerelease[2:]) + 1
         new_version = tag.replace(prerelease=f"rc{next_rc}")
     elif level == "patch":
@@ -259,9 +258,7 @@ def release(
         version: The version to release. The `v` prefix is optional
     """
     if git.is_dirty():
-        raise errors.MzConfigurationError(
-            "working directory is not clean, stash or commit your changes"
-        )
+        raise UIError("working directory is not clean, stash or commit your changes")
 
     the_tag = f"v{version}"
     confirm_version_is_next(version, affect_remote)
@@ -338,7 +335,7 @@ def update_versions_list(released_version: Version) -> None:
                 fh.write(toml_line)
                 wrote_line = True
     if not wrote_line:
-        raise errors.MzRuntimeError("Couldn't determine where to insert new version")
+        raise UIError("Couldn't determine where to insert new version")
 
 
 @cli.command()
@@ -396,7 +393,7 @@ def change_line(fname: str, line_start: str, replacement: str) -> None:
         fh.write("\n")
 
     if changes != 1:
-        raise errors.MzConfigurationError(f"Found {changes} {line_start}s in {fname}")
+        raise UIError(f"Found {changes} {line_start}s in {fname}")
 
 
 def four_years_hence() -> str:
@@ -550,7 +547,7 @@ def list_prs(recent_ref: Optional[str], ancestor_ref: Optional[str]) -> None:
         with open(creds_path) as fh:
             token = fh.read().strip()
     except FileNotFoundError:
-        raise errors.MzConfigurationError(
+        raise UIError(
             f"""No developer tool api token at {creds_path!r}
     please create an access token at https://github.com/settings/tokens"""
         )
@@ -574,11 +571,11 @@ def list_prs(recent_ref: Optional[str], ancestor_ref: Optional[str]) -> None:
                 title = contents["title"]
                 collected.append((url, title))
             except KeyError:
-                raise errors.MzRuntimeError(contents)
+                raise UIError(contents)
     for url, title in sorted(collected):
         print(url, title)
 
 
 if __name__ == "__main__":
-    with errors.error_handler(say):
+    with ui.error_handler("mkrelease"):
         cli()

misc/python/materialize/cli/mzcompose.py
@@ -24,12 +24,14 @@ code, please talk to me first!
 
 import argparse
 import os
+import subprocess
 import sys
 import webbrowser
 from pathlib import Path
 from typing import List, Optional, Sequence, Text, Tuple
 
-from materialize import errors, mzbuild, mzcompose, spawn, ui
+from materialize import mzbuild, mzcompose, spawn, ui
+from materialize.ui import UIError
 
 announce = ui.speaker("==> ")
 say = ui.speaker("")
@@ -152,24 +154,21 @@ def load_composition(args: argparse.Namespace) -> mzcompose.Composition:
     repo = load_repo(args)
     try:
         return mzcompose.Composition(repo, args.find or Path.cwd().name)
-    except errors.UnknownComposition:
+    except mzcompose.UnknownCompositionError as e:
         if args.find:
-            print(f"error: unknown composition {args.find!r}", file=sys.stderr)
-            print("hint: available compositions:", file=sys.stderr)
+            hint = "available compositions:\n"
             for name in repo.compositions:
-                print(f"    {name}", file=sys.stderr)
+                hint += f"    {name}\n"
+            e.set_hint(hint)
+            raise e
         else:
-            print(
-                "error: directory does not contain a mzcompose.yml or mzworkflows.py file",
-                file=sys.stderr,
-            )
-            print(
-                "hint: enter one of the following directories and run ./mzcompose:",
-                file=sys.stderr,
-            )
+            hint = "enter one of the following directories and run ./mzcompose:\n"
             for path in repo.compositions.values():
-                print(f"    {path.relative_to(Path.cwd())}", file=sys.stderr)
-        sys.exit(1)
+                hint += f"    {path.relative_to(Path.cwd())}\n"
+            raise UIError(
+                "directory does not contain an mzcompose.yml or mzworkflows.py",
+                hint,
+            )
 
 
 class Command:
@@ -267,7 +266,7 @@ class LintCommand(Command):
         for error in sorted(errors):
             print(error)
         if errors:
-            sys.exit(1)
+            raise UIError("lint errors discovered")
 
 
 class ListCompositionsCommand(Command):
@@ -317,14 +316,12 @@ class WebCommand(Command):
     def run(self, args: argparse.Namespace) -> None:
         composition = load_composition(args)
         ports = composition.find_host_ports(args.service)
-        if len(ports) == 1:
-            webbrowser.open(f"http://localhost:{ports[0]}")
-        elif not ports:
-            raise errors.MzRuntimeError(f"No running services matched {args.service!r}")
-        else:
-            raise errors.MzRuntimeError(
-                f"Too many ports matched {args.service!r}, found: {ports}"
+        if not ports:
+            raise UIError(
+                f"no ports discovered for service {args.service!r}",
+                hint="if the service name is valid, is it running?",
             )
+        webbrowser.open(f"http://localhost:{ports[0]}")
 
 
 class DockerComposeCommand(Command):
@@ -348,9 +345,7 @@ class DockerComposeCommand(Command):
 
     def run(self, args: argparse.Namespace) -> None:
         if args.help:
-            output = spawn.capture(
-                ["docker-compose", self.name, "--help"], stderr_too=True, unicode=True
-            )
+            output = self.capture([self.name, "--help"], stderr_too=True)
             output = output.replace("docker-compose", "./mzcompose")
             output += "\nThis command is a wrapper around Docker Compose."
             if self.help_epilog:
@@ -360,15 +355,13 @@ class DockerComposeCommand(Command):
             return
 
         # Make sure Docker Compose is new enough.
-        output = (
-            spawn.capture(["docker-compose", "version", "--short"], unicode=True)
-            .strip()
-            .strip("v")
-        )
+        output = self.capture(["version", "--short"], stderr_too=True).strip().strip("v")
         version = tuple(int(i) for i in output.split("."))
         if version < MIN_COMPOSE_VERSION:
-            msg = f"Unsupported docker-compose version: {version}, min required: {MIN_COMPOSE_VERSION}"
-            raise errors.MzConfigurationError(msg)
+            raise UIError(
+                f"unsupported docker-compose version v{output}",
+                hint=f"minimum version allowed: v{'.'.join(str(p) for p in MIN_COMPOSE_VERSION)}",
+            )
 
         composition = load_composition(args)
         announce("Collecting mzbuild dependencies")
@@ -390,11 +383,20 @@ class DockerComposeCommand(Command):
         self, args: argparse.Namespace, composition: mzcompose.Composition
     ) -> None:
         announce("Delegating to Docker Compose")
-        proc = composition.run(
+        composition.run(
             [*args.unknown_args, self.name, *args.unknown_subargs],
             check=False,
         )
-        sys.exit(proc.returncode)
+
+    def capture(self, args: List[str], stderr_too: bool = False) -> str:
+        try:
+            return spawn.capture(["docker-compose", *args], stderr_too=stderr_too, unicode=True)
+        except subprocess.CalledProcessError as e:
+            # Print any captured output, since it probably hints at the problem.
+            print(e.output, file=sys.stderr, end="")
+            raise UIError(f"running docker-compose failed (exit status {e.returncode})")
+        except FileNotFoundError:
+            raise UIError("unable to launch `docker-compose`", hint="is Docker Compose installed?")
 
 
 class RunCommand(DockerComposeCommand):
@@ -441,8 +443,11 @@ To see the available workflows, run:
             # workflow instead of Docker Compose.
             unknown_args = [*args.unknown_args, *args.unknown_subargs]
             if unknown_args:
-                raise errors.MzRuntimeError(
-                    f"cannot specify option {unknown_args[0]!r} when running a workflow"
+                raise UIError(
+                    f"unknown option {unknown_args[0]!r}",
+                    hint=f"if {unknown_args[0]!r} is a valid Docker Compose option, "
+                    f"it can't be used when running {args.workflow!r}, because {args.workflow!r} "
+                    "is a custom mzcompose workflow, not a Docker Compose service",
                 )
             workflow.run()
 
@@ -516,5 +521,5 @@ class ArgumentSubparser(argparse.ArgumentParser):
 
 
 if __name__ == "__main__":
-    with errors.error_handler(lambda *args: print(*args, file=sys.stderr)):
+    with ui.error_handler("mzcompose"):
         main(sys.argv[1:])

misc/python/materialize/errors.py
@@ -1,67 +1,0 @@
-# Copyright Materialize, Inc. and contributors. All rights reserved.
-#
-# Use of this software is governed by the Business Source License
-# included in the LICENSE file at the root of this repository.
-#
-# As of the Change Date specified in that file, in accordance with
-# the Business Source License, use of this software will be governed
-# by the Apache License, Version 2.0.
-
-"""Exceptions materialize tools may throw, and tools for handling them
-"""
-
-import sys
-from contextlib import contextmanager
-from typing import Any, Callable, Iterable
-
-
-class MzError(Exception):
-    """All errors are MzErrors"""
-
-
-class MzConfigurationError(MzError):
-    """An error that occurred because user-provided code was in error"""
-
-
-class MzRuntimeError(MzError):
-    """Execution of a task failed"""
-
-
-class UnknownItem(MzConfigurationError):
-    """A user specified something that we don't recognize"""
-
-    def __init__(self, kind: str, item: Any, acceptable: Iterable[Any]) -> None:
-        self.kind = kind
-        self.item = item
-        self.acceptable = acceptable
-
-    def __str__(self) -> str:
-        val = f"Unknown {self.kind}: '{self.item}'"
-        if self.acceptable:
-            val += ". Expected one of: " + ", ".join([str(a) for a in self.acceptable])
-        return val
-
-
-class UnknownComposition(MzError):
-    """The specified composition was unknown."""
-
-
-class BadSpec(MzConfigurationError):
-    """User provided a bad specification"""
-
-
-class Failed(MzRuntimeError):
-    """The workflow failed"""
-
-
-@contextmanager
-def error_handler(reporter: Callable[[Any], None]) -> Any:
-    """A context manager for your main"""
-    try:
-        yield
-    except MzConfigurationError as e:
-        reporter(f"ERROR: {e}")
-        sys.exit(1)
-    except MzRuntimeError as e:
-        reporter(f"Failed! {e}")
-        sys.exit(1)

misc/python/materialize/git.py
@@ -17,7 +17,7 @@ from typing import List, Optional, Set, Union
 
 import semver.version
 
-from materialize import errors, spawn
+from materialize import spawn
 
 
 def rev_count(rev: str) -> int:
@@ -52,7 +52,7 @@ def rev_parse(rev: str, *, abbrev: bool = False) -> str:
     a = ["--abbrev-ref"] if abbrev else []
     out = spawn.capture(["git", "rev-parse", *a, "--verify", rev], unicode=True).strip()
     if not out:
-        raise errors.MzRuntimeError(f"No parsed rev for {rev}")
+        raise RuntimeError(f"No parsed rev for {rev}")
     return out
 
 

test/upgrade/mzworkflows.py
@@ -13,7 +13,6 @@ from unittest.mock import patch
 
 from semver import Version, VersionInfo
 
-from materialize import errors
 from materialize.git import get_version_tags
 from materialize.mzcompose import (
     Kafka,
@@ -33,7 +32,7 @@ min_tested_tag = VersionInfo.parse(os.getenv("MIN_TESTED_TAG", "0.8.0"))
 
 all_tags = [tag for tag in get_version_tags(fetch=False) if tag.prerelease is None]
 if not all_tags:
-    raise error.MzRuntimeError(
+    raise RuntimeError(
         "No tags found in current repository. Please run git fetch --all to obtain tags."
     )
 

commit 31c18f0b3e82fd19d35e607c64b7248647edbf83

python: fix exit code on failure
Fix a bad bug in error reporting, introduced in 1e869d6, that caused
Python to exit successfully when handling a UIError.

Fix #9850.

misc/python/materialize/ui.py
@@ -194,5 +194,6 @@ def error_handler(prog: str) -> Any:
         print(f"{prog}: {fg('red')}error:{attr('reset')} {e}", file=sys.stderr)
         if e.hint:
             print(f"{attr('bold')}hint:{attr('reset')} {e.hint}")
+        sys.exit(1)
     except KeyboardInterrupt:
         sys.exit(1)